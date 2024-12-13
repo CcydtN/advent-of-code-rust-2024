f55254ee6a76a5a3c8ff7a8eb9158cb77ce4ceac
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-12-13 18:42:50 -0500

    Day 2 solved

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,11 +1,62 @@
+use itertools::Itertools;
+use num::ToPrimitive;
+
 advent_of_code::solution!(2);
 
+fn part_one_report_check(report: &[i64]) -> bool {
+    let check = if report[0] > report[1] {
+        |a, b| a - b >= 1 && a - b <= 3
+    } else {
+        |a, b| b - a >= 1 && b - a <= 3
+    };
+    report.windows(2).all(|r| check(r[0], r[1]))
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    // dbg!(input);
+    let reports = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|level| level.parse::<i64>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    reports
+        .into_iter()
+        .filter(|report| part_one_report_check(&report))
+        .count()
+        .to_u64()
+}
+
+fn part_two_report_check(report: &[i64]) -> bool {
+    // Brute force
+    for i in 0..report.len() {
+        let mut clone = report.to_vec();
+        clone.remove(i);
+        if part_one_report_check(&clone) {
+            return true;
+        }
+    }
+    false
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let reports = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|level| level.parse::<i64>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    reports
+        .into_iter()
+        .filter(|report| part_two_report_check(&report))
+        .count()
+        .to_u64()
 }
 
 #[cfg(test)]
