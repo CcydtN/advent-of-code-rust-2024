51704c9eb6d309167a5c9bc52e85ac572095aa8e
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-12-13 18:08:48 -0500

    Day 1 solved

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,37 @@
 advent_of_code::solution!(1);
+use itertools::Itertools;
+use num::ToPrimitive;
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut lists = [vec![], vec![]];
+    for (i, item) in input.split_whitespace().enumerate() {
+        lists[i % 2].push(item.parse::<u64>().ok()?);
+    }
+    // dbg!(lists);
+    let [mut left, mut right] = lists;
+    left.sort();
+    right.sort();
+    let result = left
+        .into_iter()
+        .zip(right.into_iter())
+        .map(|(a, b)| if a > b { a - b } else { b - a })
+        .sum();
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut lists = [vec![], vec![]];
+    for (i, item) in input.split_whitespace().enumerate() {
+        lists[i % 2].push(item.parse::<u64>().ok()?);
+    }
+    // dbg!(lists);
+    let [left, right] = lists;
+    let freq = right.into_iter().counts();
+    let result = left
+        .into_iter()
+        .map(|val| val * freq.get(&val).unwrap_or(&0).to_u64().unwrap())
+        .sum();
+    Some(result)
 }
 
 #[cfg(test)]
