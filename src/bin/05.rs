ec879dc8475c1dc61051f00b8fc090ff0d847946
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-12-13 21:47:49 -0500

    Day 5 solved

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,11 +1,82 @@
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+
 advent_of_code::solution!(5);
 
+fn part_one_check_order(updates: &[u64], rules: &HashMap<u64, HashSet<u64>>) -> bool {
+    let mut table = rules.get(&updates[0]).cloned().unwrap_or(HashSet::new());
+    for update in updates.into_iter().skip(1) {
+        if !table.contains(update) {
+            return false;
+        }
+        table = table.intersection(&rules[update]).cloned().collect();
+    }
+    true
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    // dbg!(input);
+    let mut parts = input.split("\n\n");
+    let mut rules = HashMap::new();
+    for line in parts.next().unwrap().lines() {
+        let sep = line.find('|').unwrap();
+        let before = line[..sep].parse::<u64>().unwrap();
+        let after = line[sep + 1..].parse::<u64>().unwrap();
+        rules.entry(before).or_insert(HashSet::new()).insert(after);
+    }
+    // dbg!(&rules);
+
+    let mut sum = 0;
+    for line in parts.next().unwrap().lines() {
+        let items = line
+            .split(",")
+            .map(|item| item.parse::<u64>().unwrap())
+            .collect_vec();
+        if part_one_check_order(&items, &rules) {
+            sum += items[items.len() / 2];
+        }
+    }
+    Some(sum)
+}
+
+fn part_two_helper(updates: Vec<u64>, rules: &HashMap<u64, HashSet<u64>>) -> Vec<u64> {
+    let updates = updates.into_iter().collect::<HashSet<u64>>();
+    let purned_rules = rules
+        .into_iter()
+        .filter(|(key, _)| updates.contains(key))
+        .map(|(&key, val)| (key, val.intersection(&updates).count()))
+        .collect::<HashMap<u64, _>>();
+    let mut tmp = purned_rules.into_iter().collect_vec();
+    tmp.sort_by(|a, b| b.1.cmp(&a.1));
+    // dbg!(&tmp);
+    tmp.into_iter().map(|x| x.0).collect_vec()
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    // dbg!(input);
+    let mut parts = input.split("\n\n");
+    let mut rules = HashMap::new();
+    for line in parts.next().unwrap().lines() {
+        let sep = line.find('|').unwrap();
+        let before = line[..sep].parse::<u64>().unwrap();
+        let after = line[sep + 1..].parse::<u64>().unwrap();
+        rules.entry(before).or_insert(HashSet::new()).insert(after);
+    }
+    // dbg!(&rules);
+
+    let mut sum = 0;
+    for line in parts.next().unwrap().lines() {
+        let items = line
+            .split(",")
+            .map(|item| item.parse::<u64>().unwrap())
+            .collect_vec();
+        let new_items = part_two_helper(items.clone(), &rules);
+        if new_items != items {
+            sum += new_items[new_items.len() / 2];
+        }
+    }
+    Some(sum)
 }
 
 #[cfg(test)]
