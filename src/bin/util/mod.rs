f4898ec36f1962925e8fb0d80afd66ac9c8b713f
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-12-16 18:39:23 -0500

    Revert "Restructure 'util' folder"
    This reverts commit c0d1dc9dc52a00ef7de96f9e6bc143d3f48dd996.

--- src/bin/util/mod.rs
+++ src/bin/util/mod.rs
@@ -1,3 +1,47 @@
-mod vector;
+use std::ops::Sub;
 
-pub(crate) use vector::Vector;
+use std::ops::Add;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub(crate) struct Vector {
+    pub(crate) x: usize,
+    pub(crate) y: usize,
+}
+
+impl Vector {
+    pub(crate) fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Add for Vector {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.x.wrapping_add(rhs.x), self.y.wrapping_add(rhs.y))
+    }
+}
+
+impl Add for &Vector {
+    type Output = Vector;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector::new(self.x.wrapping_add(rhs.x), self.y.wrapping_add(rhs.y))
+    }
+}
+
+impl Sub for Vector {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x.wrapping_sub(rhs.x), self.y.wrapping_sub(rhs.y))
+    }
+}
+
+impl Sub for &Vector {
+    type Output = Vector;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vector::new(self.x.wrapping_sub(rhs.x), self.y.wrapping_sub(rhs.y))
+    }
+}
