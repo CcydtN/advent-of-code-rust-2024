dce69a3ad5e2959f1ce48b5c1c418258f387512b
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-12-20 18:39:05 -0500

    Day 19 part 2 solved

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use num::ToPrimitive;
 
@@ -12,33 +14,47 @@ fn parse_input(input: &str) -> (Vec<&str>, Vec<&str>) {
 }
 
 // Ugly approach, but it works
-fn try_build_target<'a>(building_blocks: &[&'a str], target: &'a str) -> Option<Vec<&'a str>> {
-    if target.len() == 0 {
-        return Some(vec![]);
+fn try_build_target<'a>(
+    building_blocks: &[&'a str],
+    target: &'a str,
+    dp: &mut HashMap<&'a str, u64>,
+) -> u64 {
+    if dp.contains_key(target) {
+        return *dp.get(target).unwrap();
     }
+    let mut count = 0;
     for block in building_blocks {
         if target.starts_with(block) {
-            if let Some(mut result) = try_build_target(building_blocks, &target[block.len()..]) {
-                result.push(block);
-                return Some(result);
-            }
+            count += try_build_target(building_blocks, &target[block.len()..], dp)
         }
     }
-    None
+    dp.insert(target, count);
+    return count;
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
     let (building_blocks, targets) = parse_input(input);
     // dbg!(&building_blocks, &targets);
+    let mut dp = HashMap::new();
+    dp.insert("", 1u64);
     targets
         .into_iter()
-        .filter_map(|target| try_build_target(&building_blocks, target))
+        .map(|target| try_build_target(&building_blocks, target, &mut dp))
+        .filter(|val| val != &0)
         .count()
         .to_u64()
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (building_blocks, targets) = parse_input(input);
+    // dbg!(&building_blocks, &targets);
+    let mut dp = HashMap::new();
+    dp.insert("", 1u64);
+    targets
+        .into_iter()
+        .map(|target| try_build_target(&building_blocks, target, &mut dp))
+        .sum::<u64>()
+        .to_u64()
 }
 
 #[cfg(test)]
@@ -54,6 +70,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(16));
     }
 }
