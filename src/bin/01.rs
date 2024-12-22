3ea4f5c8e4dd88d2ef4d4ccde3f7dd494f7bd87d
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-12-22 06:57:29 -0500

    Review day 1 code, extract common part as function

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -2,36 +2,36 @@ advent_of_code::solution!(1);
 use itertools::Itertools;
 use num::ToPrimitive;
 
-pub fn part_one(input: &str) -> Option<u64> {
+fn parse_input(input: &str) -> [Vec<usize>; 2] {
     let mut lists = [vec![], vec![]];
     for (i, item) in input.split_whitespace().enumerate() {
-        lists[i % 2].push(item.parse::<u64>().ok()?);
+        lists[i % 2].push(item.parse().unwrap());
     }
+    lists
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
     // dbg!(lists);
-    let [mut left, mut right] = lists;
+    let [mut left, mut right] = parse_input(input);
     left.sort();
     right.sort();
     let result = left
         .into_iter()
         .zip(right.into_iter())
-        .map(|(a, b)| if a > b { a - b } else { b - a })
-        .sum();
-    Some(result)
+        .map(|(l, r)| l.abs_diff(r))
+        .sum::<usize>();
+    result.to_u64()
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut lists = [vec![], vec![]];
-    for (i, item) in input.split_whitespace().enumerate() {
-        lists[i % 2].push(item.parse::<u64>().ok()?);
-    }
     // dbg!(lists);
-    let [left, right] = lists;
+    let [left, right] = parse_input(input);
     let freq = right.into_iter().counts();
     let result = left
         .into_iter()
-        .map(|val| val * freq.get(&val).unwrap_or(&0).to_u64().unwrap())
-        .sum();
-    Some(result)
+        .map(|val| val * freq.get(&val).unwrap_or(&0))
+        .sum::<usize>();
+    result.to_u64()
 }
 
 #[cfg(test)]
