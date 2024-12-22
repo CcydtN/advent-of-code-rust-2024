7773dfe2dbb0e3bdbbaae4b5326f0e22503e42c2
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-12-22 03:05:06 -0500

    Day 22 part 2 solved

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -1,4 +1,10 @@
+use std::{
+    collections::{HashMap, HashSet},
+    iter,
+};
+
 use itertools::Itertools;
+use num::ToPrimitive;
 
 advent_of_code::solution!(22);
 
@@ -15,6 +21,13 @@ fn next_secret(secret: u64) -> u64 {
     let secret = prune(mix(secret, secret / 32));
     let secret = prune(mix(secret, secret * 2048));
     secret
+}
+
+fn secret_iter(mut secret: u64) -> impl Iterator<Item = u64> {
+    iter::from_fn(move || {
+        secret = next_secret(secret);
+        Some(secret)
+    })
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -35,7 +48,32 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let secrets = input
+        .split_whitespace()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect_vec();
+    let buyers = secrets.into_iter().map(|secret| {
+        secret_iter(secret)
+            .take(2000)
+            .map(|secret| (secret % 10) as i64)
+            .collect_vec()
+    });
+
+    let mut bananas = HashMap::new();
+    for prices in buyers {
+        let mut visited = HashSet::new();
+        for window in prices.windows(5) {
+            let price_change = window.windows(2).map(|x| x[1] - x[0]).collect_vec();
+            let price_change: [i64; 4] = price_change.try_into().unwrap();
+            if visited.contains(&price_change) {
+                continue;
+            }
+            visited.insert(price_change);
+            let entry = bananas.entry(price_change).or_insert(0);
+            *entry += window[4];
+        }
+    }
+    bananas.into_values().max().unwrap().to_u64()
 }
 
 #[cfg(test)]
@@ -44,13 +82,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(37327623));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(23));
     }
 }
